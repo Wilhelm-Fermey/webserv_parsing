6f7fbd1dc39014d154aec4a9e501bda53f347581
Wilhelm Fermey <fermey.wilhelm@gmail.com>
2022-11-17 15:42:59 +0100

    add all

--- main.hpp
+++ main.hpp
@@ -8,17 +8,20 @@
 # include <string>
 # include <algorithm>
 
+//////////////////// STRUCTURE POUR CHAQUE LOCATION ////////////
 struct s_location
 {
+	std::string							req_client;
     std::string							root; 
-    std::string							index;
-    std::vector<std::string>			methods;
-	std::string							auto_index;
+    std::string							path_index;
+	std::string							dir_listing;
+    std::string							methods;
 	std::map<std::string, std::string> 	redir;
 	std::map<std::string, std::string> 	cgi;
 
 };
 
+//////////////////// STRUCTURE PRINCIPALE //////////////////////
 struct s_parsing
 {
     std::string							my_port; 
@@ -26,11 +29,9 @@ struct s_parsing
     std::string							name_server;
 	int									size;
 	std::map<std::string, std::string> 	error;
-
-	std::vector<std::string>      		locations;
-
 	std::vector<s_location>      		location;
 
+	std::vector<std::string>      		locations; // aide pour parsing (ne pas utiliser).
 };
 
 //////////////////// PARSING_1 ///////////////////
@@ -45,5 +46,15 @@ void    find_port(std::vector<std::string> servers, std::vector<s_parsing> &pars
 void    find_name(std::vector<std::string> servers, std::vector<s_parsing> &parsing, int nb_serv);
 void    find_size(std::vector<std::string> servers, std::vector<s_parsing> &parsing, int nb_serv);
 void    find_error(std::vector<std::string> servers, std::vector<s_parsing> &parsing, int nb_serv);
+
+//////////////////// PARSING_3 ///////////////////
+void number_server(std::vector<std::string> servers, std::vector<s_parsing> &parsing);
+void    find_req_client(std::vector<s_parsing> &parsing);
+void    find_root(std::vector<s_parsing> &parsing);
+void    find_index(std::vector<s_parsing> &parsing);
+void    find_dir_listing(std::vector<s_parsing> &parsing);
+void    find_methods(std::vector<s_parsing> &parsing);
+void    find_redir(std::vector<s_parsing> &parsing);
+void    find_cgi(std::vector<s_parsing> &parsing);
 
 #endif
--- parsing3.cpp
+++ parsing3.cpp
@@ -1,5 +1,5 @@
 #include "main.hpp"
-/*
+
 void number_server(std::vector<std::string> servers, std::vector<s_parsing> &parsing)
 {
 	int 	nb_loc;
@@ -23,7 +23,7 @@ void number_server(std::vector<std::string> servers, std::vector<s_parsing> &par
 		for (int j = 0; j < tab_len.size() -1; j++) // découpe les loc pour mettre dans tableau de loc
 			tab_loc.push_back(servers[i].substr(tab_len[j], tab_len[j +1] - tab_len[j]));
 
-		parsing[i].locations = tab_loc; 
+		parsing[i].locations = tab_loc;
 
 		for (int j = 0; j < nb_loc; j++) // creation du bon nombre de struct location dans struct parsing.
 			parsing[i].location.push_back(s_location());
@@ -227,87 +227,4 @@ void	find_cgi(std::vector<s_parsing> &parsing)
 			}
 		}
 	}
-}
-*/
-
-int	main(void)
-{
-	std::string 				file;
-	int							nb_serv;
-	std::vector<int>			tab_len; 
-	std::vector<std::string>    servers; // .conf cut par server
-	std::vector<s_parsing>		parsing; // info parsing
-
-	////////////////////////////////////////////////////////////////
-	/////////////////////        SERVER       //////////////////////
-	////////////////////////////////////////////////////////////////
-
-	file = ft_read_file("file.conf");
-	nb_serv = number_server(file, tab_len);
-	cut_server(file, tab_len, servers);
-	if (create_struct(parsing, nb_serv))
-		return (1);
-	find_ip(servers, parsing, nb_serv);
-	find_port(servers, parsing, nb_serv);
-	find_name(servers, parsing, nb_serv);
-	find_size(servers, parsing, nb_serv);
-	find_error(servers, parsing, nb_serv);
-
-	/*..............................................................................*/
-	/*.............. Loop to print info in parsing struct (string) .................*/
-	/*..............................................................................*/
-
-	for (int i = 0; i < parsing.size(); i++)
-		std::cout << parsing[i].my_port <<std::endl;
-
-	/*..............................................................................*/
-	/*.............. Loop to print info in parsing struct (map) ....................*/
-	/*..............................................................................*/
-/*
-	for (int i = 0; i < parsing.size(); i++)
-	{
-		std::map<std::string, std::string>::iterator it = parsing[i].error.begin();
-		for (; it != parsing[i].error.end(); it++ )
-			std::cout << it->first << ' ' << it->second << std::endl;
-	}
-*/
-
-	////////////////////////////////////////////////////////////////
-	//////////////////         LOCATION         ////////////////////
-	////////////////////////////////////////////////////////////////
-
-	number_server(servers, parsing);
-	find_req_client(parsing);
-	find_root(parsing);
-	find_index(parsing);
-	find_dir_listing(parsing);
-	find_methods(parsing);
-	find_redir(parsing);
-	find_cgi(parsing);
-
-	/*..............................................................................*/
-	/*.............. Loop to print info in location struct (string) ................*/
-	/*..............................................................................*/
-/*
-	for (int i = 0; i < parsing.size(); i++)
-	{
-		for (int j = 0; j < parsing[i].location.size(); j++)
-			std::cout << parsing[i].location[j].req_client << std::endl;
-	}
-*/
-	/*..............................................................................*/
-	/*................ Loop to print info in location struct (map) .................*/
-	/*..............................................................................*/
-
-/*
-	for (int i = 0; i < parsing.size(); i++)
-	{
-		for (int j = 0; j < parsing[i].location.size(); j++)
-		{
-			std::map<std::string, std::string>::iterator it = parsing[i].location[j].cgi.begin();
-			for (; it != parsing[i].location[j].cgi.end(); it++ )
-				std::cout << it->first << ' ' << it->second << std::endl;
-		}
-	}
-*/
 }
